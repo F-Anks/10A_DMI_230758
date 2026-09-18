9a0918d3c8a2a2d2b3f87c961c2445db53b0b463
Francisco Garcia Garcia <franciscogarciagarcia911@gmail.com>
2026-09-18 16:19:57 -0600

    feat(practica-03): implementar base de yes_no_app y pantalla de chat
    Renombrar proyecto anterior a Practica02

---
+++ Practica03/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: const AssetImage('src/menchow.jpg'),
+          ),
+        ),
+        title: Text('amigo'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return MyMessageBubble();
+                },
+              ),
+            ),
+
+            /// Caja de texto de mensajes
+          ],
+        ),
+      ),
+    );
+  }
+}
